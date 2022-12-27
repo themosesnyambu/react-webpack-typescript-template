160ec69bfe44003cee0e52266ec85d5dcaa4a03e
themosesnyambu <nyamosekitando@gmail.com>
2022-12-27 19:24:01 +0300

    initial config with routing

---
+++ src/app.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const Home = () => <div>Home</div>;
+const BlogPage = () => <div>blogs</div>;
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="about" element={<BlogPage />} />
+    </Routes>
+  );
+}
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  devtool: "inline-source-map",
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+};
